0e54bf89f3ccb9acfd9a1ccd92e956d1928bb439
BlissArmah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2022-12-05 08:43:40 -0800

    add

--- TS_PROGRESS/intro/src/index.ts
+++ TS_PROGRESS/intro/src/index.ts
@@ -22,14 +22,79 @@
 // calculateTax(40_000)
 
 // OBJECT
-let employee: {
-    readonly id: number,
-    name: string,
-    retire: (date : Date) => void
-} = { 
-    id: 1,
-     name: 'Bliss',
-     retire: (date : Date) =>{
-        console.log(date)
-     }
-     }
+// let employee: {
+//     readonly id: number,
+//     name: string,
+//     retire: (date : Date) => void
+// } = { 
+//     id: 1,
+//      name: 'Bliss',
+//      retire: (date : Date) =>{
+//         console.log(date)
+//      }
+//      }
+
+//  TYPE ALIASES
+
+// type Employee = {
+
+//     readonly id: number,
+//     name: string,
+//     retire: (date : Date) => void
+// }
+
+// let employee: Employee = {
+//     id: 1,
+//      name: 'Bliss',
+//      retire: (date : Date) =>{
+//         console.log(date)
+//      }
+//      }
+
+// UNION TYPES
+// function kgTolbs(weight:number | string) : number {
+// // Narroowing
+// if(typeof weight === 'number')
+// return weight * 15
+// else{
+//    return parseInt(weight) * 2.2
+// }
+// }
+// kgTolbs(0)
+// kgTolbs("hi")
+
+// INTERSECTION TYPE
+// type Draggable = {
+//     drag: () => void
+// }
+
+// type Rezisable = {
+//     resize: () => void
+// }
+
+// type UIWidget = Draggable & Rezisable
+
+// let textbox: UIWidget = {
+//     drag: () => { },
+//     resize: () => { }
+// }
+
+// Classes
+
+class Account{
+    id:number
+    owner: string
+    balance: number
+    constructor(id:number, owner: string, balance: number){
+        this.id=id
+        this.owner=owner
+        this.balance=balance
+    }
+
+    deposit(amount:number):void{
+        if (amount<=0) {
+            throw new Error('amount is invalid');
+        }
+        this.balance += amount
+    }
+}
